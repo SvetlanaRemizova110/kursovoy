d4dd3ecb4540c3cc5940b2be2c95df136db64b55
Remizova Svetlana <svetlanarem192@gmail.com>
2025-04-01 23:05:49 +0300

    Closed #2

--- kursovoy/import.Designer.cs
+++ kursovoy/import.Designer.cs
@@ -31,14 +31,19 @@ namespace kursovoy
         {
             this.btnRestoreDatabase = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCSVFile = new System.Windows.Forms.TextBox();
+            this.comboBoxTables = new System.Windows.Forms.ComboBox();
+            this.btnImportData = new System.Windows.Forms.Button();
+            this.btnSelectCsv = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnRestoreDatabase
             // 
             this.btnRestoreDatabase.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnRestoreDatabase.Location = new System.Drawing.Point(52, 48);
+            this.btnRestoreDatabase.Location = new System.Drawing.Point(12, 54);
             this.btnRestoreDatabase.Name = "btnRestoreDatabase";
-            this.btnRestoreDatabase.Size = new System.Drawing.Size(169, 78);
+            this.btnRestoreDatabase.Size = new System.Drawing.Size(358, 42);
             this.btnRestoreDatabase.TabIndex = 7;
             this.btnRestoreDatabase.TabStop = false;
             this.btnRestoreDatabase.Text = "Восстановление структуры БД";
@@ -48,7 +53,7 @@ namespace kursovoy
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(81, 193);
+            this.button1.Location = new System.Drawing.Point(12, 242);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 34);
             this.button1.TabIndex = 9;
@@ -57,16 +62,73 @@ namespace kursovoy
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 23);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Выбранный файл";
+            // 
+            // txtCSVFile
+            // 
+            this.txtCSVFile.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtCSVFile.Location = new System.Drawing.Point(163, 185);
+            this.txtCSVFile.Name = "txtCSVFile";
+            this.txtCSVFile.Size = new System.Drawing.Size(207, 30);
+            this.txtCSVFile.TabIndex = 14;
+            // 
+            // comboBoxTables
+            // 
+            this.comboBoxTables.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxTables.FormattingEnabled = true;
+            this.comboBoxTables.Location = new System.Drawing.Point(12, 126);
+            this.comboBoxTables.Name = "comboBoxTables";
+            this.comboBoxTables.Size = new System.Drawing.Size(159, 31);
+            this.comboBoxTables.TabIndex = 13;
+            // 
+            // btnImportData
+            // 
+            this.btnImportData.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnImportData.Location = new System.Drawing.Point(159, 242);
+            this.btnImportData.Name = "btnImportData";
+            this.btnImportData.Size = new System.Drawing.Size(211, 34);
+            this.btnImportData.TabIndex = 11;
+            this.btnImportData.TabStop = false;
+            this.btnImportData.Text = "Импорт CSV-файла";
+            this.btnImportData.UseVisualStyleBackColor = true;
+            this.btnImportData.Click += new System.EventHandler(this.btnImportData_Click);
+            // 
+            // btnSelectCsv
+            // 
+            this.btnSelectCsv.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSelectCsv.Location = new System.Drawing.Point(177, 126);
+            this.btnSelectCsv.Name = "btnSelectCsv";
+            this.btnSelectCsv.Size = new System.Drawing.Size(193, 34);
+            this.btnSelectCsv.TabIndex = 12;
+            this.btnSelectCsv.TabStop = false;
+            this.btnSelectCsv.Text = "Выбор CSV-файла";
+            this.btnSelectCsv.UseVisualStyleBackColor = true;
+            this.btnSelectCsv.Click += new System.EventHandler(this.btnSelectCsv_Click);
+            // 
             // import
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(401, 317);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCSVFile);
+            this.Controls.Add(this.comboBoxTables);
+            this.Controls.Add(this.btnImportData);
+            this.Controls.Add(this.btnSelectCsv);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnRestoreDatabase);
             this.Name = "import";
             this.Text = "import";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,5 +136,10 @@ namespace kursovoy
 
         private System.Windows.Forms.Button btnRestoreDatabase;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCSVFile;
+        private System.Windows.Forms.ComboBox comboBoxTables;
+        private System.Windows.Forms.Button btnImportData;
+        private System.Windows.Forms.Button btnSelectCsv;
     }
 }
